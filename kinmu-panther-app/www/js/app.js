6971435307273feaa0df387246620430c99347fc
yamato-satoshi <yamato-satoshi@sji-inc.jp>
2016-01-23 11:29:28 +0900

    日だけをシンプルにリスト表示

--- kinmu-panther-app/www/js/app.js
+++ kinmu-panther-app/www/js/app.js
@@ -3,7 +3,17 @@
   
   var module = ons.bootstrap('app', ['onsen']);
   module.controller('AppController', function($scope){
-    $scope.currentMonth = new Date();
+    var now = new Date();
+    
+    var days = [];
+    var date = new Date(now.getFullYear(), now.getMonth(), 1);
+    while (date.getMonth() == now.getMonth()) {
+      days.push(new Date(date.getTime()));
+      date.setDate(date.getDate() + 1);
+    }
+    
+    $scope.now = now;
+    $scope.days = days;
   });
   
   ons.ready(function() {

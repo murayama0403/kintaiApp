0a7db095161e4f001ad18133f8564a77117ae0d0
yamato-satoshi <yamato-satoshi@sji-inc.jp>
2016-01-24 13:25:26 +0900

    土日の色変更

--- kinmu-panther-app/www/js/app.js
+++ kinmu-panther-app/www/js/app.js
@@ -3,12 +3,30 @@
   
   var module = ons.bootstrap('app', ['onsen']);
   module.controller('AppController', function($scope){
+  
+    function calcDayType(date) {
+      // TODO 祝日
+      if (date.getDay() == 0) {
+        return 'sun'
+      }
+      if (date.getDay() == 6) {
+        return 'sat'
+      }
+      return ''
+    }
+    
     var now = new Date();
     
     var days = [];
     var date = new Date(now.getFullYear(), now.getMonth(), 1);
     while (date.getMonth() == now.getMonth()) {
-      days.push(new Date(date.getTime()));
+      var currentDate = new Date(date.getTime())
+      var dayData = {
+        date: currentDate,
+        dayType: calcDayType(currentDate)
+      };
+      console.info(dayData);
+      days.push(dayData);
       date.setDate(date.getDate() + 1);
     }
     

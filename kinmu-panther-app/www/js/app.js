cf78476bbcc34b9036249fd97ca1405027b21a84
yamato-satoshi <yamato-satoshi@sji-inc.jp>
2016-01-25 18:45:18 +0900

    祝日と就業規則の休日判定追加

--- kinmu-panther-app/www/js/app.js
+++ kinmu-panther-app/www/js/app.js
@@ -3,16 +3,31 @@
   
   var module = ons.bootstrap('app', ['onsen']);
   module.controller('AppController', function($scope){
-  
+
+    var SUNDAY = 0;
+    var SATURDAY = 6;
+    // 就業規則で12/30-1/3は休み(1/1は元日なので設定不要)
+    var COMPANY_HOLIDAYS = ['12/30', '12/31', '1/2', '1/3'];
+    
     function calcDayType(date) {
-      // TODO 祝日
-      if (date.getDay() == 0) {
-        return 'sun'
+      var holiday = calcHoliday(date);
+      if (holiday != '') {
+        return 'hol';
       }
-      if (date.getDay() == 6) {
-        return 'sat'
+      
+      // Dateのmonthは0始まりなので1を足して人間にわかりやすい文字列で判定
+      var md = (date.getMonth() + 1) + '/' + date.getDate();
+      if (COMPANY_HOLIDAYS.indexOf(md) != -1) {
+        return 'hol';
       }
-      return ''
+      
+      if (date.getDay() == SUNDAY) {
+        return 'sun';
+      }
+      if (date.getDay() == SATURDAY) {
+        return 'sat';
+      }
+      return '';
     }
     
     var now = new Date();
@@ -20,12 +35,11 @@
     var days = [];
     var date = new Date(now.getFullYear(), now.getMonth(), 1);
     while (date.getMonth() == now.getMonth()) {
-      var currentDate = new Date(date.getTime())
+      var currentDate = new Date(date.getTime());
       var dayData = {
         date: currentDate,
         dayType: calcDayType(currentDate)
       };
-      console.info(dayData);
       days.push(dayData);
       date.setDate(date.getDate() + 1);
     }

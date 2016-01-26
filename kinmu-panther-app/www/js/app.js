86365218d92307889e2702d56fb6ffc767a0d813
tomishima-yusuke <tomishima-yusuke@sji-inc.jp>
2016-01-27 00:15:08 +0900

    土、日曜の色変更

--- kinmu-panther-app/www/js/app.js
+++ kinmu-panther-app/www/js/app.js
@@ -1,32 +1,42 @@
-(function(){
+﻿(function(){
   'use strict';
   
   var module = ons.bootstrap('app', ['onsen']);
   module.controller('AppController', function($scope){
     $scope.currentMonth = new Date();
+    
+    // 日付のリスト
     $scope.dateList = {
+    
+      // itemScopeの設定
       configureItemScope: function(index, itemScope) {
+        var indexDay = index; // 本来は+1 TODO:なおす
         var indexYear = $scope.currentMonth.getFullYear();
         var indexMonth = $scope.currentMonth.getMonth();
-        var indexDate = new Date( indexYear, indexMonth, index );
+        var indexDate = new Date( indexYear, indexMonth, indexDay );
         var weekTbl = new Array('日','月','火','水','木','金','土');
-        var indexDay = indexDate.getDay();
-        var indexDayKanji = weekTbl[indexDay];
+        var indexDayOfWeek = indexDate.getDay();
+        var indexDayKanji = weekTbl[indexDayOfWeek];
         var color = 'weekDay';
-        if (indexDay == 1) {
+        if (indexDayOfWeek == 0) {
           color = 'sunday';
-        } else if (indexDay == 7) {
+        } else if (indexDayOfWeek == 6) {
           color = 'saturday';
         }
+        // アイテムを入れる
         itemScope.item = {
-          name: index,
+          name: indexDay,
           day: indexDayKanji,
           color: color
         };
       },
+      
+      // アイテムの高さ
       calculateItemHeight: function(index) {
         return 45;
       },
+      
+      // アイテムの数
       countItems: function() {
         var year = $scope.currentMonth.getFullYear();
         var month = $scope.currentMonth.getMonth() + 1;
@@ -47,9 +57,11 @@
           return 32;
         }
       },
+      
       destroyItemScope: function(index, scope) {
         console.log("Destroyed item #" + index);
       }
+      
     };
   });
   ons.ready(function() {

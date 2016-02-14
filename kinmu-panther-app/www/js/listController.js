a6ebf2a87f798bbe534e5c8ddbcf674cb22387aa
yamato-satoshi <yamato-satoshi@sji-inc.jp>
2016-02-15 08:32:16 +0900

    #5 リスト内勤怠時間クリックでの処理実装（処理はまだ固定値入れるだけ）

--- kinmu-panther-app/www/js/listController.js
+++ kinmu-panther-app/www/js/listController.js
@@ -4,4 +4,13 @@ angular.module('app')
   var now = new Date();
   this.data = kinmuService.getMonthData(now.getFullYear(), now.getMonth() + 1);
   
+  this.inputIn = function(dayData) {
+	  // TODO 入力欄表示
+	  dayData.inTime = '09:00';
+  };
+  
+  this.inputOut = function(dayData) {
+	  // TODO 入力欄表示
+	  dayData.outTime = '17:45';
+  };
 }]);

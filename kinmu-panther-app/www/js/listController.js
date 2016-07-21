0d83574cd762c085219bac9f5f1836926ce0b67e
yamato-satoshi <yamato-satoshi@sji-inc.jp>
2016-07-21 21:04:21 +0900

    カレンダー表示をがんばってみた。

--- kinmu-panther-app/www/js/listController.js
+++ kinmu-panther-app/www/js/listController.js
@@ -2,6 +2,38 @@ angular.module('app')
 .controller('ListController', ['kinmuService', function(kinmuService){
 
   var now = new Date();
-  this.data = kinmuService.getMonthData(now.getFullYear(), now.getMonth() + 1);
+  var monthData = kinmuService.getMonthData(now.getFullYear(), now.getMonth() + 1);
   
+  this.calendarData = createCalendarData(monthData);
+  
+  function createCalendarData(data) {
+    var weeks = [];
+  
+    var week = [];
+    addDummys(week, data.days[0].date);
+  
+    var daySize = data.days.length;
+    for (var i = 0; i < daySize; i++) {
+      week.push(data.days[i]);
+    
+      if (week.length == 7 || i + 1 == daySize) {
+        weeks.push(week);
+        week = [];
+      }
+    }
+    
+    return {
+      year: data.year,
+      month: data.month,
+      weeks: weeks
+    };
+  }
+  
+  function addDummys(week, firstDate) {
+    var firstDay = firstDate.getDay();
+    var startDummys = [];
+    for (var i = 0; i < firstDay; i++) {
+      week.push(i);
+    }
+  }
 }]);

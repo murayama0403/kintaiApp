339cd7cd02860d3b38bcd7d4d44ecdbe4b3f4a6d
ishizaki-kentarou <ishizaki-kentarou@sji-inc.jp>
2016-08-04 19:11:01 +0900

    右寄せ 最終週のダミー追加

--- kinmu-panther-app/www/js/listController.js
+++ kinmu-panther-app/www/js/listController.js
@@ -16,12 +16,21 @@ angular.module('app')
     for (var i = 0; i < daySize; i++) {
       week.push(data.days[i]);
     
-      if (week.length == 7 || i + 1 == daySize) {
+      if (week.length == 7 ) {
+        weeks.push(week);
+        week = [];
+      }
+      if ( i + 1 == daySize) {
+	    var monthIndex = now.getMonth() ;
+	    var date = new Date(now.getFullYear(), monthIndex, 1);
+	    var Lastdate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+	    addDummysLast(week, Lastdate);
         weeks.push(week);
         week = [];
       }
     }
-    
+
     return {
       year: data.year,
       month: data.month,
@@ -36,4 +45,35 @@ angular.module('app')
       week.push(i);
     }
   }
+  function addDummysLast(week, LastDate) {
+    var LastDay = LastDate.getDate();
+    var LastDummys = [];
+    var zanDay = 0;
+    switch (LastDate.getDay()){
+    case 0:
+     zanDay = 6;
+     break;
+    case 1:
+     zanDay = 5;
+     break;
+    case 2:
+     zanDay = 4;
+     break;
+    case 3:
+     zanDay = 3;
+     break;
+    case 4:
+     zanDay = 2;
+     break;
+    case 5:
+     zanDay = 1;
+     break;
+    }
+
+    for (var i = LastDay; i < LastDay + zanDay; i++) {
+      week.push(i);
+    }
+
+  }
+
 }]);

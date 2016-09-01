21c769c4a8aa74d72bdbc9bd4b79536f8fb78b4f
yamato-satoshi <yamato-satoshi@sji-inc.jp>
2016-09-02 08:42:29 +0900

    データバインディング設定

--- kinmu-panther-app/www/js/inputController.js
+++ kinmu-panther-app/www/js/inputController.js
@@ -2,9 +2,12 @@ angular.module('app')
 .controller('InputController', ['timeDialogService', function(timeDialogService){
 
 	this.currentDate = new Date();
+	this.inTime = '';
+	this.outTime = '';
+	this.quickTime = '現在時刻';
 	
 //	this.inputTime = function(type, dayData) {
 //		timeDialogService.showDialog(type, dayData);
 //	};
-  
+	
 }]);

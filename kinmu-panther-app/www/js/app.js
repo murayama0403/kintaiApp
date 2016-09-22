9208e2ccdda82fe76974fe59ebdf3037394791a8
yamato-satoshi <yamato-satoshi@sji-inc.jp>
2016-09-22 11:47:30 +0900

    ngStorageは全データをロードしてしまって効率が悪いのでangular-local-strageを使うように変更

--- kinmu-panther-app/www/js/app.js
+++ kinmu-panther-app/www/js/app.js
@@ -1,9 +1,13 @@
-(function(){
-  'use strict';
-  
-  ons.bootstrap('app', ['onsen', 'ngStorage']);
-  
-  ons.ready(function() {
-    // Init code here
-  });
+(function() {
+    'use strict';
+
+    ons.bootstrap('app', ['onsen', 'LocalStorageModule']);
+
+    angular.module('app').config(['localStorageServiceProvider', function(localStorageServiceProvider) {
+        localStorageServiceProvider.setPrefix('kinmuPanther');
+    }]);
+
+    ons.ready(function() {
+        // Init code here
+    });
 })();
--- kinmu-panther-app/www/js/kinmuService.js
+++ kinmu-panther-app/www/js/kinmuService.js
@@ -1,19 +1,19 @@
-angular.module('app')
-.service('kinmuService', ['$localStorage', function($localStorage){
-	
-	this.TIME_EMPTY = '';
-	this.TIME_OFF = '---';
-	
-	var currentMonth = '201609';
-	var query = {};
-	query[currentMonth] = {};
-	this.data = $localStorage.$default(query);
-	
-	console.info(this.data);
+angular.module('app').service('kinmuService', ['localStorageService', function(localStorageService){
+    
+    this.TIME_EMPTY = '';
+    this.TIME_OFF = '---';
+    
+    var currentMonth = '201609';
+    
+    this.data = localStorageService.get(currentMonth);
+    
+    console.info(this.data);
 
-	$localStorage[currentMonth] = {8: {
-		in: '09:00',
-		out: '17:45'
-	}};
+    localStorageService.set(currentMonth, {
+        8: {
+            in: '09:00',
+            out: '17:45'
+        }
+    });
 
 }]);

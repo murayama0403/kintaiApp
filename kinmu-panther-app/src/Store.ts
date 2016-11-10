bb1a73758a992fc545bc8486bab9f35e3306d0a5
yamato-satoshi <yamato-satoshi@sji-inc.jp>
2016-11-10 15:58:02 +0900

    Redux DevTools extension を使えるようにした

--- kinmu-panther-app/src/Store.ts
+++ kinmu-panther-app/src/Store.ts
@@ -1,8 +1,11 @@
 import { kintai } from './Reducer'
 import { createStore, combineReducers } from 'redux'
 
+declare var window: any
+
 export default createStore(
     combineReducers({
         kintai
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );

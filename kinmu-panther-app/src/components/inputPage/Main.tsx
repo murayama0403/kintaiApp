969763f9ffcf65fa7c99e7ab8b679000acca66a7
yamato-satoshi <yamato-satoshi@sji-inc.jp>
2016-12-14 08:00:49 +0900

    リファクタリング

--- kinmu-panther-app/src/components/inputPage/Main.tsx
+++ kinmu-panther-app/src/components/inputPage/Main.tsx
@@ -1,57 +1,22 @@
 import * as React from "react"
 import {GlobalState, getDayKintai} from "../../States"
 import {DispatchActions} from "../../DispatchActions"
-import {List, ListItem} from 'material-ui/List'
-import IconButton from 'material-ui/IconButton'
-import ActionSchedule from 'material-ui/svg-icons/action/schedule'
-import ActionUpdate from 'material-ui/svg-icons/action/update'
+import List from 'material-ui/List'
 import {TimeInput, IN, OUT} from "./TimeInput"
-import * as _ from "lodash"
 
 interface Props {
     value: GlobalState;
     actions: DispatchActions;
 }
 
-const listInnerStyle = {
-    paddingLeft: "4px",
-    paddingRight: "4px",
-    paddingTop: "0px",
-    paddingBottom: "0px"
-}
-
 export class Main extends React.Component<Props, {}> {
-    private inButtons =
-            <div>
-                <IconButton onClick={this.handleInNow.bind(this)}>
-                    <ActionUpdate />
-                </IconButton>
-                <IconButton>
-                    <ActionSchedule />
-                </IconButton>
-            </div>
-
-    private outButtons =
-            <div>
-                <IconButton onClick={this.handleOutNow.bind(this)}>
-                    <ActionUpdate />
-                </IconButton>
-                <IconButton>
-                    <ActionSchedule />
-                </IconButton>
-            </div>
-
     render() {
         const currentKintai = getDayKintai(this.props.value.kintai, this.props.value.inputPage.currentDate)
 
         return (
             <List>
-                <ListItem innerDivStyle={listInnerStyle} rightIconButton={this.inButtons} disabled={true}>
-                    <TimeInput type={IN} value={currentKintai.inTime} onSelected={this.handleInSelected.bind(this)} />
-                </ListItem>
-                <ListItem innerDivStyle={listInnerStyle} rightIconButton={this.outButtons} disabled={true}>
-                    <TimeInput type={OUT} value={currentKintai.outTime} onSelected={this.handleOutSelected.bind(this)} />
-                </ListItem>
+                <TimeInput type={IN} value={currentKintai.inTime} onSelected={this.handleInSelected.bind(this)} />
+                <TimeInput type={OUT} value={currentKintai.outTime} onSelected={this.handleOutSelected.bind(this)} />
             </List>
         )
     }
@@ -64,27 +29,4 @@ export class Main extends React.Component<Props, {}> {
         this.props.actions.selectOut(this.props.value.inputPage.currentDate, value)
     }
 
-    private handleInNow() {
-        const time = this.calcInTime(new Date())
-        this.props.actions.selectIn(this.props.value.inputPage.currentDate, time)
-    }
-
-    private handleOutNow() {
-        const time = this.calcOutTime(new Date())
-        this.props.actions.selectOut(this.props.value.inputPage.currentDate, time)
-    }
-
-    private calcInTime(now: Date) {
-        now.setMinutes(now.getMinutes() + 15);
-        return this.to15minTime(now)
-    }
-
-    private calcOutTime(now: Date) {
-        return this.to15minTime(now)
-    }
-
-    private to15minTime(time: Date) {
-        time.setMinutes(Math.floor(time.getMinutes() / 15) * 15);
-        return time.getHours().toString() + ':' + _.padStart(time.getMinutes().toString(), 2, '0')
-    }
 }
--- kinmu-panther-app/src/components/inputPage/TimeInput.tsx
+++ kinmu-panther-app/src/components/inputPage/TimeInput.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import {DispatchActions} from "../../DispatchActions"
 import SelectField from 'material-ui/SelectField'
+import {ListItem} from 'material-ui/List'
 import MenuItem from 'material-ui/MenuItem'
+import IconButton from 'material-ui/IconButton'
+import ActionSchedule from 'material-ui/svg-icons/action/schedule'
+import ActionUpdate from 'material-ui/svg-icons/action/update'
 import {TouchTapEvent} from 'material-ui'
 import * as _ from "lodash"
 
@@ -11,21 +15,31 @@ interface Props {
     onSelected: (value: string) => void;
 }
 
+interface InputType {
+    label: string;
+    menus: Array<any>
+    offsetMinutes: number
+}
+
 const TIMES = [':00', ':15', ':30', ':45']
 
 export const IN: InputType = {
     label: "出勤",
-    menus: createMenus("9:00")
+    menus: createMenus("9:00"),
+    offsetMinutes: 15
 }
 
 export const OUT: InputType = {
     label: "退勤",
-    menus: createMenus("17:45")
+    menus: createMenus("17:45"),
+    offsetMinutes: 0
 }
 
-interface InputType {
-    label: string;
-    menus: Array<any>
+const listInnerStyle = {
+    paddingLeft: "4px",
+    paddingRight: "4px",
+    paddingTop: "0px",
+    paddingBottom: "0px"
 }
 
 function createMenus(defaultValue: string): Array<any> {
@@ -44,21 +58,44 @@ function createMenus(defaultValue: string): Array<any> {
 }
 
 export class TimeInput extends React.Component<Props, {}> {
+    private quickButtons =
+            <div>
+                <IconButton onClick={this.handleNow.bind(this)}>
+                    <ActionUpdate />
+                </IconButton>
+                <IconButton>
+                    <ActionSchedule />
+                </IconButton>
+            </div>
+
     render() {
         return (
-            <SelectField
-                hintText={this.props.type.label}
-                value={this.props.value}
-                onChange={this.handleChange.bind(this)}
-                style={{width: "200px"}}
-                >
-                {this.props.type.menus}
-            </SelectField>
+            <ListItem innerDivStyle={listInnerStyle} rightIconButton={this.quickButtons} disabled={true}>
+                <SelectField
+                    hintText={this.props.type.label}
+                    value={this.props.value}
+                    onChange={this.handleChange.bind(this)}
+                    style={{width: "200px"}}
+                    >
+                    {this.props.type.menus}
+                </SelectField>
+            </ListItem>
         )
     }
 
     private handleChange(event: TouchTapEvent, index: number, value: string) {
         event.preventDefault()
         this.props.onSelected(value)
+    }
+
+    private handleNow() {
+        const time = this.calcNowTime(new Date())
+        this.props.onSelected(time)
+    }
+
+    private calcNowTime(now: Date) {
+        now.setMinutes(now.getMinutes() + this.props.type.offsetMinutes);
+        now.setMinutes(Math.floor(now.getMinutes() / 15) * 15);
+        return now.getHours().toString() + ':' + _.padStart(now.getMinutes().toString(), 2, '0')
     }
 }

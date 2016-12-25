2ad96b480b3238a32a454fba1e2d633115c61237
yamato-satoshi <yamato-satoshi@sji-inc.jp>
2016-12-26 08:51:04 +0900

    BottomNavigationを追加（未動作）

--- kinmu-panther-app/src/components/App.tsx
+++ kinmu-panther-app/src/components/App.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import {GlobalState} from "../States";
 import {DispatchActions} from "../DispatchActions"
-import {InputPage} from "./inputPage/InputPage";
+import {InputPage} from "./inputPage/InputPage"
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation'
+import Divider from 'material-ui/Divider'
+import EditIcon from 'material-ui/svg-icons/image/edit'
+import ListIcon from 'material-ui/svg-icons/action/list'
 
 interface Props {
     value: GlobalState;
@@ -11,10 +15,22 @@ interface Props {
 export class App extends React.Component<Props, {}> {
     render() {
         return (
-            <div>
+            <div style={{height: "100vh", display: "flex", justifyContent: "space-between", flexDirection: "column"}} >
                 <InputPage {...this.props} />
+                <div>
+                    <Divider />
+                    <BottomNavigation selectedIndex={0}>
+                        <BottomNavigationItem label="入力"
+                            icon={<EditIcon />}
+                            onTouchTap={() => console.log('input')}
+                        />
+                        <BottomNavigationItem label="一覧"
+                            icon={<ListIcon />}
+                            onTouchTap={() => console.log('list')}
+                        />
+                    </BottomNavigation>
+                </div>
             </div>
         )
     }
-
 }

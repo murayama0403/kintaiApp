210395cf78bd643f4e8ed3a8cb6338f659a95ef9
soukani <song-jiani@sji-inc.jp>
2017-05-13 22:30:06 +0900

    アイコンとマージン修正

--- src/components/inputPage/Main.tsx
+++ src/components/inputPage/Main.tsx
@@ -1,10 +1,11 @@
 import { TouchTapEvent } from "material-ui"
 import RaisedButton from "material-ui/RaisedButton"
-import HomeIcon from "material-ui/svg-icons/action/home"
-import MorningRunIcon from "material-ui/svg-icons/maps/directions-run"
+import CallHomeIcon from "material-ui/svg-icons/action/settings-phone"
+import BatteryChargIcon from "material-ui/svg-icons/device/battery-charging-50"
+import MorningExerciseIcon from "material-ui/svg-icons/maps/directions-run"
 import CafeIcon from "material-ui/svg-icons/maps/local-cafe"
 import RestrantIcon from "material-ui/svg-icons/maps/restaurant-menu"
-import SleepIcon from "material-ui/svg-icons/notification/airline-seat-flat"
+
 import WorkIcon from "material-ui/svg-icons/notification/airline-seat-recline-normal"
 import TextField from "material-ui/TextField"
 import * as React from "react"
@@ -15,12 +16,13 @@ import { IN, OUT, TimeInput } from "./TimeInput"
 export class Main extends React.Component<RootProps, {}> {
     public render() {
         const currentKintai = getDayKintaiOrDefault(this.props.value.kintai, this.props.value.view.currentDate)
+        const restButtonStyle = { height: "70px", margin: 5}
 
         const rest2Icon = currentKintai.noRest2 ? <WorkIcon /> : <CafeIcon />
         const rest3Icon = currentKintai.noRest3 ? <WorkIcon /> : <RestrantIcon />
-        const rest4Icon = currentKintai.noRest4 ? <WorkIcon /> : <HomeIcon />
-        const rest5Icon = currentKintai.noRest5 ? <WorkIcon /> : <SleepIcon />
-        const rest6Icon = currentKintai.noRest6 ? <WorkIcon /> : <MorningRunIcon />
+        const rest4Icon = currentKintai.noRest4 ? <WorkIcon /> : <CallHomeIcon />
+        const rest5Icon = currentKintai.noRest5 ? <WorkIcon /> : <BatteryChargIcon />
+        const rest6Icon = currentKintai.noRest6 ? <WorkIcon /> : <MorningExerciseIcon />
 
         return (
             <div className="content">
@@ -32,39 +34,40 @@ export class Main extends React.Component<RootProps, {}> {
                     type={OUT}
                     value={currentKintai.outTime}
                     onSelected={(value) => this.handleOutSelected(value)} />
-
+             
                 <div>
                     <RaisedButton
-                        label="17:45-18:00"
+                        label={<div style = {{textAlign: "center"}}>17:45<br />18:00</div>}
                         icon={rest2Icon}
                         secondary={currentKintai.noRest2}
-                        onTouchTap={(event) => this.handleRest2Toggle(event)} />&nbsp;
-        
-                    <RaisedButton
-                        label="19:30-20:00"
+                        onTouchTap={(event) => this.handleRest2Toggle(event)}
+                        style={restButtonStyle}/>
+                   <RaisedButton
+                        label={<div style = {{textAlign: "center"}}>19:30<br />20:00</div>}
                         icon={rest3Icon}
                         secondary={currentKintai.noRest3}
-                        onTouchTap={(event) => this.handleRest3Toggle(event)} />&nbsp;
-						
+                        onTouchTap={(event) => this.handleRest3Toggle(event)}
+                        style={restButtonStyle}/>
                     <RaisedButton
-                        label="22:30-23:00"
+                        label={<div style = {{textAlign: "center"}}>22:30<br />23:00</div>}
                         icon={rest4Icon}
                         secondary={currentKintai.noRest4}
-                        onTouchTap={(event) => this.handleRest4Toggle(event)} />&nbsp;
-					
+                        onTouchTap={(event) => this.handleRest4Toggle(event)}
+                        style={restButtonStyle}/>
                     <RaisedButton
-                        label="2:30-3:00"
+                        label={<div style = {{textAlign: "center"}}>02:30<br />03:00</div>}
                         icon={rest5Icon}
                         secondary={currentKintai.noRest5}
-                        onTouchTap={(event) => this.handleRest5Toggle(event)} />&nbsp;
-						
+                        onTouchTap={(event) => this.handleRest5Toggle(event)}
+                        style={restButtonStyle}/>
                     <RaisedButton
-                        label="8:30-9:00"
+                        label={<div style = {{textAlign: "center"}}>8:30<br />9:00</div>}
                         icon={rest6Icon}
                         secondary={currentKintai.noRest6}
-                        onTouchTap={(event) => this.handleRest6Toggle(event)} />&nbsp;
+                        onTouchTap={(event) => this.handleRest6Toggle(event)}
+                        style={restButtonStyle}/>
                 </div>
-
+                        	
                 <TextField
                     multiLine={true}
                     fullWidth={true}
